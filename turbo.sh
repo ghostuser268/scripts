ba0827d017c7744273acbfa540cf11ada3455451
maahase singh <maahasesingh@protonmail.com>
2022-06-11 19:33:13 +0800

    scripts

---
+++ turbo.sh
@@ -0,0 +1,15 @@
+#! /bin/sh
+
+boost=/sys/devices/system/cpu/cpufreq/boost
+switch=$(cat ${boost}) 
+
+echo "0" | sudo tee $boost
+
+#1 on 
+#0 off
+#if [[ $switch == '0' ]]; then
+	#echo "1" | sudo tee $boost
+#else
+	#echo "0" | sudo tee $boost
+#fi
+
